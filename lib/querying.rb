725c5dd94e34b1529107dc227fa327104be9c6ca
Joseph Sesti <sestijoey63@gmail.com>
2019-10-24 17:36:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,9 +14,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species
+  "SELECT species,
   COUNT(*) FROM characters
-  GROUP BY species,
+  GROUP BY species
   ORDER BY COUNT(species)
   DESC LIMIT 1;"
 end
